c131e3a9541f2c44ae7878166da3eea9a9067974
AdBorkowska <ad.borkowska96@gmail.com>
2021-07-02 21:27:38 +0200

    changed time widget

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-//clock
-
-/*setInterval(setClock, 1000)
-
-const hourHand = document.getElementById("hour")
-const minuteHand = document.getElementById("minute")
-const secondHand = document.getElementById("second")
-
-function setClock() {
-    const currentDate = new Date();
-    const secondsRatio = currentDate.getSeconds() / 60;
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
+//local time
+function getTime() {
+    const dateObj = new Date;
+    let time = dateObj.getHours() + ":" + dateObj.getMinutes();
+    document.getElementById("localTime").innerHTML = `Your local time is: ${time}`
 }
 
-function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio * 360)
-}
+setInterval(getTime, 1000);
 
-setClock();*/
+getTime();
 
 //date
-function getTime() {
+function getDate() {
     let dateObj = new Date();
     let month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
     let date = ('0' + dateObj.getDate()).slice(-2);
@@ -32,9 +19,9 @@ function getTime() {
     
     document.getElementById("date").innerHTML = `${d}`;
 }
-setInterval(getTime, 1000);
+setInterval(getDate, 1000);
 
-getTime();
+getDate();
 
 //weather
 const myKey = "af4cddf8ba6d2ac99ba304abc62d2cc7";
@@ -45,45 +32,39 @@ searchCity.addEventListener('keypress', setQuery);
 function setQuery(ev) {
     if (ev.keyCode == 13) {
         getResult(searchCity.value);
+        document.getElementById("city").innerHTML = `${searchCity.value}, `
         console.log(searchCity.value)
     }
 }
 
 function getResult(query) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${myKey}`)
-.then ((resp) => resp.json())
-.then(function (data) {
-let weather = data["weather"]["0"]["description"]
-let weatherIcon = data["weather"]["0"]["icon"];
-console.log(weather);
+    .then ((resp) => resp.json())
+    .then(function (data) {
+    let weather = data["weather"]["0"]["description"]
+    let weatherIcon = data["weather"]["0"]["icon"];
+    console.log(weather);
+    console.log(data);
     document.getElementById('showWX').innerHTML = `${((data.main.temp - 273.15).toFixed(1))} &#x2103` 
     document.getElementById('showdescription').innerHTML = `${weather}` 
     document.getElementById('weather-icon').innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">`;
+    //searched city time: calculating timezone
+    let timezone = data["timezone"]
+    let d = new Date()
+    let localTime = d.getTime()
+    let localOffset = d.getTimezoneOffset() * 60000
+    let utc = localTime + localOffset
+    let selectedCity = utc + (1000 * timezone)
+    var nd = new Date(selectedCity)
+    let currentDate = new Date(nd);
+    console.log(currentDate)
+    //showing results
+    let searchedTime = nd.getHours() + ":" + nd.getMinutes();
+    document.getElementById("searchedTime").innerHTML = `Time for selected city: ${searchedTime}`
+    //searched city name and country
+    let countryCode = data["sys"]["country"];
+    console.log(countryCode);
+    document.getElementById("country").innerHTML = `${countryCode}`
 })
-.then(updateClock)
 }
 
-function updateClock() {
-    
-    setInterval(setClock, 1000)
-
-const hourHand = document.getElementById("hour")
-const minuteHand = document.getElementById("minute")
-const secondHand = document.getElementById("second")
-
-function setClock() {
-    const currentDate = new Date();
-    const secondsRatio = currentDate.getSeconds() / 60;
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
-}
-
-function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio * 360)
-}
-
-setClock();
-}
